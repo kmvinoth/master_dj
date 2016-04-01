d07522cd820d24101472a9956e6626648fbe54ad
kmvinoth@gmail.com <kmvinoth@gmail.com>
2016-04-01 16:41:17 +0200

    Mastered Views and UrlConfs

--- books/views.py
+++ books/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse    # HttpResponse is a class
+import _datetime  # built in date time
 
 # Create your views here.
+# Function based views
+# Homepage
+
+
+def home(request):
+    return HttpResponse("Homepage")
+
+# First view (static)
+
+
+def hello(request):
+    return HttpResponse("Django Powered Page")
+
+# Second view (dynamic content but URL Static)
+
+
+def current_date_time(request):
+    now = _datetime.datetime.now()
+    html = "<html><body> It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+# Third View (Dynamic URL's with dynamic content)
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404
+    dt = _datetime.datetime.now() + _datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
